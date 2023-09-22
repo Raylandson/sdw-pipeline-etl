a750d185ea5d873a39be3ac22d52eaf716851551
Raylandson Cesário <62724100+Raylandson@users.noreply.github.com>
2023-09-22 01:36:47 -0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,5 @@
 import json
-import openai
 import requests
-
-my_openai_api_key = "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"
-openai.api_key = my_openai_api_key
 
 headers = {
     "Authorization": "Bearer YOUR_KEY"
@@ -27,16 +23,6 @@ def generate_ai_news(user):
     result = json.loads(response.text)
 
     return result["openai"]["generated_text"] if response.status_code == 200 else ""
-
-
-def generate_news_id(news: list):
-    new_id = 0
-
-    for ids_numbers in news:
-        new_id += 1
-
-
-    return new_id if new_id != 0 else 1
 
 
 for costumer in data["costumers"]:
